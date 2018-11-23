17f382d9d79ad4f5704dfb3583f3b11b650d7512
Emil <1161162012@mail.ru>
2018-11-23 19:58:09 +0300

    start of project

---
+++ app/src/main/java/com/example/searchandrescue/Authorization.java
@@ -0,0 +1,32 @@
+package com.example.searchandrescue;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Authorization extends Fragment {
+
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView;
+        rootView = inflater.inflate(R.layout.fragment_authorization, container, false);
+
+        return rootView;
+    }
+
+}
