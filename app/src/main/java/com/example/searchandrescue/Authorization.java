1729a4604e9cd15cbfcecbea0d42470de34af904
Emil <1161162012@mail.ru>
2018-11-24 00:53:31 +0300

    add methods for users sing in and sing up

--- app/src/main/java/com/example/searchandrescue/Authorization.java
+++ app/src/main/java/com/example/searchandrescue/Authorization.java
@@ -1,24 +1,119 @@
 package com.example.searchandrescue;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Objects;
 
 public class Authorization extends Fragment {
 
-    /*@Override
+    private String mLogin;
+    private String mPassword;
+    private FirebaseAuth mAuth;
+    FirebaseUser mUser;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+
+    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
-*/
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        //View rootView = inflater.inflate(R.layout.fragment_authorization, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_authorization, container, false);
 
-        return inflater.inflate(R.layout.fragment_authorization, container, false);
+        Button singIn = (Button) rootView.findViewById(R.id.singIn); // кнопка авторизации
+        singIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mLogin = ((EditText) getActivity().findViewById(R.id.login)).getText().toString();
+                mPassword = ((EditText) getActivity().findViewById(R.id.password)).getText().toString();
+                if("".equals(mLogin) || "".equals(mPassword)) {
+                    Toast.makeText(getActivity(), "Одно из полей не заполненно. Пожалуйста, заполните все поля и повторите отправку", Toast.LENGTH_LONG).show();
+                }
+                else {
+                singInUser();
+                }
+
+            }
+        });
+
+        Button singUp = (Button) rootView.findViewById(R.id.singUp); // кнопка регистрации
+        singUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mLogin = ((EditText) getActivity().findViewById(R.id.login)).getText().toString();
+                mPassword = ((EditText) getActivity().findViewById(R.id.password)).getText().toString();
+                if("".equals(mLogin) || "".equals(mPassword)) {
+                    Toast.makeText(getActivity(), "Одно из полей не заполненно. Пожалуйста, заполните все поля и повторите отправку", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    addUser();
+                }
+            }
+        });
+        return rootView;
     }
 
+    private void addUser(){
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
+        Task<AuthResult> authResultTask = mAuth.createUserWithEmailAndPassword(mLogin, mPassword).addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(getContext(), "Регистрация успешна", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getContext(), "Регистрация провалена", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    private void singInUser(){
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if(user != null){
+                    // sing in
+
+                }
+                else {
+                    // sing out
+                }
+            }
+        };
+
+        mAuth.signInWithEmailAndPassword(mLogin, mPassword).addOnCompleteListener(Objects.requireNonNull(getActivity()), new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()) {
+                    Toast.makeText(getContext(), "Авторизация успешна", Toast.LENGTH_LONG).show();
+                }
+                else{
+                    Toast.makeText(getContext(), "Авторизация провалена", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
 }
