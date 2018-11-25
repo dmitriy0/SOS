1ef63f5830346ba2bb74d32baf48124f10716543
DanilaSuvorov <dansuvtim@gmail.com>
2018-11-25 14:19:49 +0300

    auth

--- app/src/main/java/com/example/searchandrescue/Volunteer.java
+++ app/src/main/java/com/example/searchandrescue/Volunteer.java
@@ -1,6 +1,7 @@
 package com.example.searchandrescue;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.net.Uri;
@@ -11,6 +12,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
@@ -98,5 +100,7 @@ public class Volunteer extends Fragment {
         });
 
     }
+
+
 
 }
